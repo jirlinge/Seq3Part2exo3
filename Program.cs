3eef2fd02d60784adc904adcb3c96cecc27ab735
jirlinge <julienirlinger@gmail.com>
2021-02-16 16:54:07 +0100

    version final

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("veuillez renseigner votre age : ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("etes vous un homme ou une femme ? ");
+            string sexe = Console.ReadLine();
+
+            if (age > 18)
+            {
+                Console.WriteLine($"vous etes {sexe} et vous etes majeur ");
+            }
+
+            else
+            {
+                Console.Write($"vous etes {sexe} et vous etes mineur ");
+            }
+
+
+
+
+        }
+    }
+}
